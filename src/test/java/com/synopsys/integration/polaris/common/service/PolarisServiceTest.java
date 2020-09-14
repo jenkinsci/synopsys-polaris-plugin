cf2e5487be32ae7bcf6df0d104bea4ae92b144be
Richard Otte <rotte@blackducksoftware.com>
2020-09-14 14:31:42 -0400

    test: Re-implementing testGetAll in PolarisServiceTest

--- src/test/java/com/synopsys/integration/polaris/common/service/PolarisServiceTest.java
+++ src/test/java/com/synopsys/integration/polaris/common/service/PolarisServiceTest.java
@@ -1,22 +1,31 @@
 package com.synopsys.integration.polaris.common.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 import org.opentest4j.AssertionFailedError;
 
+import com.google.gson.Gson;
 import com.synopsys.integration.exception.IntegrationException;
+import com.synopsys.integration.log.LogLevel;
+import com.synopsys.integration.log.PrintStreamIntLogger;
+import com.synopsys.integration.polaris.common.api.PolarisResource;
+import com.synopsys.integration.polaris.common.api.model.JobAttributes;
 import com.synopsys.integration.polaris.common.request.PolarisRequestFactory;
 import com.synopsys.integration.polaris.common.rest.AccessTokenPolarisHttpClient;
 import com.synopsys.integration.rest.HttpUrl;
@@ -26,39 +35,42 @@ import com.synopsys.integration.rest.support.UrlSupport;
 
 public class PolarisServiceTest {
     public static String PAGE_ONE_OFFSET = "0";
-    public static String PAGE_TWO_OFFSET = "25";
-    public static String PAGE_THREE_OFFSET = "50";
+    public static String PAGE_TWO_OFFSET = "1";
+    public static String PAGE_THREE_OFFSET = "2";
+    public static String PAGE_FOUR_OFFSET = "3";
 
-    private HttpUrl BASE_URL = new HttpUrl("https://google.com");
-    private UrlSupport urlSupport = new UrlSupport();
+    private final HttpUrl BASE_URL = new HttpUrl("https://google.com");
+    private final UrlSupport urlSupport = new UrlSupport();
 
     public PolarisServiceTest() throws IntegrationException {
     }
 
     private static Stream<Arguments> createGetAllMockData() {
         Map<String, String> getAllOnOnePageMap = new HashMap<>();
-        getAllOnOnePageMap.put(PAGE_ONE_OFFSET, "projects_all_on_one_page.json");
+        getAllOnOnePageMap.put(PAGE_ONE_OFFSET, "jobs_all_on_one_page.json");
 
         Map<String, String> getAllMultiPageMap = new HashMap<>();
-        getAllMultiPageMap.put(PAGE_ONE_OFFSET, "projects_page_1_of_3.json");
-        getAllMultiPageMap.put(PAGE_TWO_OFFSET, "projects_page_2_of_3.json");
-        getAllMultiPageMap.put(PAGE_THREE_OFFSET, "projects_page_3_of_3.json");
+        getAllMultiPageMap.put(PAGE_ONE_OFFSET, "jobs_page_1_of_3.json");
+        getAllMultiPageMap.put(PAGE_TWO_OFFSET, "jobs_page_2_of_3.json");
+        getAllMultiPageMap.put(PAGE_THREE_OFFSET, "jobs_page_3_of_3.json");
 
-        Map<String, String> lessProjectsThanExpectedMap = new HashMap<>(getAllMultiPageMap);
-        lessProjectsThanExpectedMap.remove(PAGE_THREE_OFFSET);
+        Map<String, String> lessThanExpectedMap = new HashMap<>(getAllMultiPageMap);
+        lessThanExpectedMap.remove(PAGE_THREE_OFFSET);
 
-        Map<String, String> changingTotalMap = new HashMap<>(getAllMultiPageMap);
-        changingTotalMap.put(PAGE_TWO_OFFSET, "projects_page_2_of_2.json");
+        Map<String, String> moreThanExpectedMap = new HashMap<>(getAllMultiPageMap);
+        moreThanExpectedMap.put(PAGE_TWO_OFFSET, "jobs_page_2_of_4.json");
+        moreThanExpectedMap.put(PAGE_THREE_OFFSET, "jobs_page_3_of_4.json");
+        moreThanExpectedMap.put(PAGE_FOUR_OFFSET, "jobs_page_4_of_4.json");
 
         Map<String, String> duplicatedDataMap = new HashMap<>(getAllMultiPageMap);
-        duplicatedDataMap.put(PAGE_TWO_OFFSET, "projects_page_1_of_3.json");
+        duplicatedDataMap.put(PAGE_TWO_OFFSET, "jobs_page_1_of_3.json");
 
         return Stream.of(
-            Arguments.of(getAllOnOnePageMap, 16),
-            Arguments.of(getAllMultiPageMap, 66),
-            Arguments.of(lessProjectsThanExpectedMap, 50),
-            Arguments.of(changingTotalMap, 66),
-            Arguments.of(duplicatedDataMap, 66)
+            Arguments.of(getAllOnOnePageMap, 3, 3),
+            Arguments.of(getAllMultiPageMap, 1, 3),
+            Arguments.of(lessThanExpectedMap, 1, 2),
+            Arguments.of(moreThanExpectedMap, 1, 3),
+            Arguments.of(duplicatedDataMap, 1, 3)
         );
     }
 
@@ -95,33 +107,24 @@ public class PolarisServiceTest {
     }
  */
 
-    // TODO: Replace with an api we use
-    /*
-       @ParameterizedTest
-       @MethodSource("createGetAllMockData")
-       public void testGetAll(Map<String, String> offsetsToResults, int expectedTotal) throws IntegrationException {
-           HttpUrl requestUri = urlSupport.appendRelativeUrl(BASE_URL, PolarisService.PROJECT_API_SPEC);
+    @ParameterizedTest
+    @MethodSource("createGetAllMockData")
+    public void testGetAll(Map<String, String> offsetsToResults, int pageSize, int expectedTotal) throws IntegrationException {
+        HttpUrl requestUri = urlSupport.appendRelativeUrl(BASE_URL, "/api/jobs");
 
-           AccessTokenPolarisHttpClient polarisHttpClient = Mockito.mock(AccessTokenPolarisHttpClient.class);
-           mockClientBehavior(polarisHttpClient, requestUri, offsetsToResults, "projects_no_more_results.json");
+        AccessTokenPolarisHttpClient polarisHttpClient = Mockito.mock(AccessTokenPolarisHttpClient.class);
+        mockClientBehavior(polarisHttpClient, requestUri, offsetsToResults, "jobs_no_more_results.json");
 
-           PolarisJsonTransformer polarisJsonTransformer = new PolarisJsonTransformer(PolarisServicesFactory.createDefaultGson(), new PrintStreamIntLogger(System.out, LogLevel.INFO));
+        PolarisJsonTransformer polarisJsonTransformer = new PolarisJsonTransformer(new Gson(), new PrintStreamIntLogger(System.out, LogLevel.INFO));
+        PolarisService polarisService = new PolarisService(polarisHttpClient, polarisJsonTransformer, pageSize);
+        try {
+            List<PolarisResource<JobAttributes>> allPagesResponse = polarisService.getAll(requestUri, JobAttributes.class);
+            assertEquals(expectedTotal, allPagesResponse.size());
+        } catch (IntegrationException e) {
+            fail("Mocked response caused PolarisService::GetAllResponses to throw an unexpected IntegrationException, which should never happen in this test.", e);
+        }
+    }
 
-           PolarisPagedRequestCreator requestCreator = (limit, offset) -> PolarisRequestFactory.createDefaultPagedRequestBuilder(limit, offset)
-                                                                              .url(requestUri)
-                                                                              .build();
-
-           PolarisPagedRequestWrapper pagedRequestWrapper = new PolarisPagedRequestWrapper(requestCreator, ProjectV0Resources.class);
-
-           PolarisService polarisService = new PolarisService(polarisHttpClient, polarisJsonTransformer, PolarisRequestFactory.DEFAULT_LIMIT);
-           try {
-               List<ProjectV0Resource> allPagesResponse = polarisService.getAllResponses(pagedRequestWrapper);
-               assertEquals(expectedTotal, allPagesResponse.size());
-           } catch (IntegrationException e) {
-               fail("Mocked response caused PolarisService::GetAllResponses to throw an unexpected IntegrationException, which should never happen in this test.", e);
-           }
-       }
-   */
     private void mockClientBehavior(AccessTokenPolarisHttpClient polarisHttpClient, HttpUrl url, Map<String, String> offsetsToResults, String emptyResultsPage) {
         try {
             for (Map.Entry<String, String> entry : offsetsToResults.entrySet()) {
